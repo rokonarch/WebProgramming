05b96d1794c315f086220cc29dd7e4cd8d57ca07
rokonarch <developerrokon229@gmail.com>
2024-05-16 20:54:52 +0600

    Create Vriable.js

---
+++ Vriable.js
@@ -0,0 +1,16 @@
+//Variable Declaration
+var name;
+let age;
+const PI = 3.14;
+//Variable Assignment:
+name = "Raton";
+age = 30;
+
+console.log(name);
+console.log(age);
+console.log(PI);
+
+
+//Booleans
+let mybool=true;
+console.log(mybool);
