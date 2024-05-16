cbc3f9679fd1a1e4ede1275d52714f9ca5a635f9
rokonarch <developerrokon229@gmail.com>
2024-05-16 20:59:50 +0600

    Create ConditionalStatement

---
+++ ConditionalStatement.js
@@ -0,0 +1,44 @@
+let x = 10;
+
+if (x > 5) {
+    console.log("x is greater than 5");
+}
+
+
+let x = 10;
+
+if (x > 5) {
+    console.log("x is greater than 5");
+} else {
+    console.log("x is not greater than 5");
+}
+
+let x = 10;
+
+if (x > 10) {
+    console.log("x is greater than 10");
+} else if (x < 10) {
+    console.log("x is less than 10");
+} else {
+    console.log("x is equal to 10");
+}
+let x = 10;
+let y = 5;
+
+if (x > 5) {
+    if (y > 2) {
+        console.log("Both x and y are greater than their respective thresholds");
+    }
+}
+let fruit = "apple";
+
+switch (fruit) {
+    case "banana":
+        console.log("It's a banana");
+        break;
+    case "apple":
+        console.log("It's an apple");
+        break;
+    default:
+        console.log("Unknown fruit");
+}
